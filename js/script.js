69222e262a90e3d85b8942b5bb2125a9df46b91d
Nick Muenchow and Ryan Harvey <ryan@>
2016-03-21 16:15:29 -0700

    Created operation functionalitty.  Also allows user to perform second operation, with or without the result of the first in the new operation.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,7 @@
-$(document).ready(function() {
-  $("h1").click(function() {
-    alert("This is the H1 yo");
+//For some reason, this is broken now.  It can't do anything except addition.  You gotta fix this bruh.
 
-  });
-});
+// Functions
 
-
-
-
-
-/*
 var add = function(number1, number2){
   return number1 + number2
 };
@@ -19,36 +11,71 @@ var subtract = function(number1, number2){
 var multiply = function(number1, number2){
   return number1 * number2
 };
-var dividify = function(number1, number2){
+var divide = function(number1, number2){
   return number1 / number2
 };
 
-var firstNumber = parseInt(prompt("Give me a number"));
-var secondNumber = parseInt(prompt("Give me another number"));
-alert(add(firstNumber, secondNumber));
+var maths = function (mathType, number1, number2){
+  if(mathType === "ADDITION"){
+    answer = add(number1, number2);
 
-var mathType = prompt("What Kind of Operation Will You Do?  Please type 'addition', 'subtraction',
-'multiplication' or 'division'.");
+  } else if(mathType === "SUBTRACTION"){
+    answer = subtract(number1, number2);
 
-if(mathType === "addition"){
+  } else if(mathType === "MULTIPLICATION"){
+    answer = multiply(number1, number2);
 
-  var firstNumber = parseInt(prompt("Give me a number"));
-  var secondNumber = parseInt(prompt("Give me another number"));
-  alert(add(firstNumber, secondNumber));
-
-} elseif(mathType === "subtraction"){
-  var firstNumber = parseInt(prompt("Give me a number"));
-  var secondNumber = parseInt(prompt("Give me another number"));
-  alert(subtract(firstNumber, secondNumber));
-
-} elseif(mathType === "multiplication"){
-  var firstNumber = parseInt(prompt("Give me a number"));
-  var secondNumber = parseInt(prompt("Give me another number"));
-  alert(multiply(firstNumber, secondNumber));
-
-} elseif(mathType === "division"){
-  var firstNumber = parseInt(prompt("Give me a number"));
-  var secondNumber = parseInt(prompt("Give me another number"));
-  alert(dividify(firstNumber, secondNumber));
+  } else if(mathType === "DIVISION"){
+    answer = divide(number1, number2);
+  }
+  return answer;
 }
-*/
+
+//Start Prompts
+var mathType = prompt("What Kind of Operation Will You Do?  Please type 'addition', 'subtraction', 'multiplication' or 'division'.");
+var mathType = mathType.toUpperCase();
+
+if(mathType !== "ADDITION" && mathType !== "SUBTRACTION" && mathType !== "MULTIPLICATION" && mathType !== "DIVISION"){
+  alert("Sorry that is not a valid answer");
+}else{
+  var firstNumber = parseInt(prompt("Give me a number"));
+  var secondNumber = parseInt(prompt("Give me another number"));
+  maths(mathType, firstNumber, secondNumber);
+  alert("The Answer Is: " + answer);
+}
+
+//Second Operation Prompt
+var response = prompt("Do You Want to Perform Another Operation?");
+response = response.toUpperCase();
+  if (response === "YES"){
+    //Do you Want To Use Previous Answer in Calculation
+    var response2 = prompt("Do You Want to Use Your Previous answer in your calculation?")
+    response2 = response2.toUpperCase();
+    //If YES
+    if (response2 === "YES"){
+      mathType = prompt("Your current number is " + answer + ". What operation would you like to perform?");
+      mathType = mathType.toUpperCase()
+      if(mathType !== "ADDITION" && mathType !== "SUBTRACTION" && mathType !== "MULTIPLICATION" && mathType !== "DIVISION"){
+        alert("Sorry that is not a valid answer");
+      }else{
+        var secondNumber = parseInt(prompt("Give me another number"));
+        maths(mathType, answer, secondNumber);
+        alert("The Answer Is: " + answer);
+      }
+    //IF NO
+    }else{
+      mathType = prompt("What operation would you like to perform?");
+      mathType = mathType.toUpperCase();
+      if(mathType !== "ADDITION" && mathType !== "SUBTRACTION" && mathType !== "MULTIPLICATION" && mathType !== "DIVISION"){
+        alert("Sorry that is not a valid answer");
+      }else{
+        var firstNumber = parseInt(prompt("Give me a number"));
+        var secondNumber = parseInt(prompt("Give me another number"));
+        maths(mathType, firstNumber, secondNumber);
+        alert("The Answer Is: " + answer);
+      }
+    }
+  // QUIT OUT
+  }else{
+    alert("FINE.");
+  }
