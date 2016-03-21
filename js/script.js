74777050ea2ad96783e37188e405564da0c16754
Nick Muenchow and Ryan Harvey <ryan@>
2016-03-21 12:03:30 -0700

    setup the javascript, worked on if else code

--- js/script.js
+++ js/script.js
@@ -1,4 +1,54 @@
-var add = function(number1, number2){return number1 + number2};
+$(document).ready(function() {
+  $("h1").click(function() {
+    alert("This is the H1 yo");
+
+  });
+});
+
+
+
+
+
+/*
+var add = function(number1, number2){
+  return number1 + number2
+};
+var subtract = function(number1, number2){
+  return number1 - number2
+};
+var multiply = function(number1, number2){
+  return number1 * number2
+};
+var dividify = function(number1, number2){
+  return number1 / number2
+};
+
 var firstNumber = parseInt(prompt("Give me a number"));
 var secondNumber = parseInt(prompt("Give me another number"));
 alert(add(firstNumber, secondNumber));
+
+var mathType = prompt("What Kind of Operation Will You Do?  Please type 'addition', 'subtraction',
+'multiplication' or 'division'.");
+
+if(mathType === "addition"){
+
+  var firstNumber = parseInt(prompt("Give me a number"));
+  var secondNumber = parseInt(prompt("Give me another number"));
+  alert(add(firstNumber, secondNumber));
+
+} elseif(mathType === "subtraction"){
+  var firstNumber = parseInt(prompt("Give me a number"));
+  var secondNumber = parseInt(prompt("Give me another number"));
+  alert(subtract(firstNumber, secondNumber));
+
+} elseif(mathType === "multiplication"){
+  var firstNumber = parseInt(prompt("Give me a number"));
+  var secondNumber = parseInt(prompt("Give me another number"));
+  alert(multiply(firstNumber, secondNumber));
+
+} elseif(mathType === "division"){
+  var firstNumber = parseInt(prompt("Give me a number"));
+  var secondNumber = parseInt(prompt("Give me another number"));
+  alert(dividify(firstNumber, secondNumber));
+}
+*/
