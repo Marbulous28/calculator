f1c912125635fae17bb2c5a02af630a5745b5166
Nick Muenchow and Ryan Harvey <nick@>
2016-03-21 16:52:46 -0700

    whatever

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
-//For some reason, this is broken now.  It can't do anything except addition.  You gotta fix this bruh.
 
-// Functions
+//Functions
 
 var add = function(number1, number2){
   return number1 + number2
@@ -32,45 +31,43 @@ var maths = function (mathType, number1, number2){
 }
 
 //Start Prompts
-var mathType = prompt("What Kind of Operation Will You Do?  Please type 'addition', 'subtraction', 'multiplication' or 'division'.");
-var mathType = mathType.toUpperCase();
+var typePrompt = "What Kind of Operation Would You Like To Perform?  Please type 'addition', 'subtraction', 'multiplication' or 'division'."
+var mathType = prompt(typePrompt).toUpperCase();
+
 
 if(mathType !== "ADDITION" && mathType !== "SUBTRACTION" && mathType !== "MULTIPLICATION" && mathType !== "DIVISION"){
   alert("Sorry that is not a valid answer");
 }else{
-  var firstNumber = parseInt(prompt("Give me a number"));
-  var secondNumber = parseInt(prompt("Give me another number"));
+  var firstNumber = parseInt(prompt("Enter A Number: "));
+  var secondNumber = parseInt(prompt("Enter Another Number: "));
   maths(mathType, firstNumber, secondNumber);
   alert("The Answer Is: " + answer);
 }
 
 //Second Operation Prompt
-var response = prompt("Do You Want to Perform Another Operation?");
-response = response.toUpperCase();
+var response = prompt("Do You Want to Perform Another Operation?").toUpperCase();
   if (response === "YES"){
     //Do you Want To Use Previous Answer in Calculation
     var response2 = prompt("Do You Want to Use Your Previous answer in your calculation?")
     response2 = response2.toUpperCase();
     //If YES
     if (response2 === "YES"){
-      mathType = prompt("Your current number is " + answer + ". What operation would you like to perform?");
-      mathType = mathType.toUpperCase()
+      mathType = prompt("Your current number is " + answer + ". " + typePrompt).toUpperCase();
       if(mathType !== "ADDITION" && mathType !== "SUBTRACTION" && mathType !== "MULTIPLICATION" && mathType !== "DIVISION"){
         alert("Sorry that is not a valid answer");
       }else{
-        var secondNumber = parseInt(prompt("Give me another number"));
+        var secondNumber = parseInt(prompt("Enter Another Number: "));
         maths(mathType, answer, secondNumber);
         alert("The Answer Is: " + answer);
       }
     //IF NO
     }else{
-      mathType = prompt("What operation would you like to perform?");
-      mathType = mathType.toUpperCase();
+      mathType = prompt(typePrompt).toUpperCase();
       if(mathType !== "ADDITION" && mathType !== "SUBTRACTION" && mathType !== "MULTIPLICATION" && mathType !== "DIVISION"){
         alert("Sorry that is not a valid answer");
       }else{
-        var firstNumber = parseInt(prompt("Give me a number"));
-        var secondNumber = parseInt(prompt("Give me another number"));
+        var firstNumber = parseInt(prompt("Enter A Number: "));
+        var secondNumber = parseInt(prompt("Enter Another Number: "));
         maths(mathType, firstNumber, secondNumber);
         alert("The Answer Is: " + answer);
       }
