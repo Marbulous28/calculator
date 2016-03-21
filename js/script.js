17e5360766506a64443c112d8c3f99e1372e3eb5
Nick Muenchow and Ryan Harvey <ryan@>
2016-03-21 09:53:41 -0700

    Added initial file structure.  Created initial js with addition functionality.

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var add = function(number1, number2){return number1 + number2};
+var firstNumber = parseInt(prompt("Give me a number"));
+var secondNumber = parseInt(prompt("Give me another number"));
+alert(add(firstNumber, secondNumber));
