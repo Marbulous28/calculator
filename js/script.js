a65c67d179f996509bdd17cbfd3731aac7ca6594
Peter Beach and Ryan Harvey <ryan@>
2016-03-23 11:34:09 -0700

    calculator website, added some interface.

--- js/script.js
+++ js/script.js
@@ -14,6 +14,17 @@ var divide = function(number1, number2){
   return number1 / number2
 };
 
+$(document).ready(function(){
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
+
+/*
 var maths = function (mathType, number1, number2){
   if(mathType === "ADDITION"){
     answer = add(number1, number2);
@@ -76,3 +87,5 @@ var response = prompt("Do You Want to Perform Another Operation?").toUpperCase()
   }else{
     alert("FINE.");
   }
+
+*/
